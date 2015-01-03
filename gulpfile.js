e05f2c27f87b32b3d45589917fcacd04c223cb3f
fundon <cfddream@gmail.com>
2015-01-04 00:04:51 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,14 @@
 
 var gulp = require('gulp');
 var deploy = require('gulp-gh-pages');
+var jade = require('gulp-jade');
 
 gulp.task('default', function () {
+  return gulp.src('src/**/*.jade')
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('deploy', function () {
